5f0324147debf4c12aa9c57defe6b3961944692f
shawngmc <shawngmc@gmail.com>
2021-05-13 15:16:56 -0400

    Docusaurus

---
+++ docusaurus.config.js
@@ -0,0 +1,79 @@
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'Shawngmc\'s Tech Ref Links',
+  tagline: 'I find neat geeky things. I keep them here.',
+  url: 'https://techlinks.hitoma.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'shawngmc', // Usually your GitHub org/user name.
+  projectName: 'tech-ref-links', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'Tech-Ref-Links',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'doc',
+          docId: 'links',
+          position: 'left',
+          label: 'Links',
+        },
+        {to: '/blog', label: 'Blog', position: 'left'},
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Links',
+              to: '/docs/links',
+            },
+          ],
+        },
+        {
+          title: 'Other',
+          items: [
+            {
+              label: 'Blog',
+              to: '/blog',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Shawn McNaughton; Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+  plugins: [
+    require.resolve('@cmfcmf/docusaurus-search-local')
+  ]
+};
